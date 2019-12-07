39c73f7eef711c2f06151f3b72439cc06f554271
rajaramanwork <rajaraman.work@gmail.com>
2019-12-07 12:33:59 -0500

    Integrated Procedure Calls

--- pythonutils.testharness/test_oracle.py
+++ pythonutils.testharness/test_oracle.py
@@ -4,18 +4,13 @@ import logger.loggingutils as logger
 import oracle.oracleutils as oracle_utils
 import oracle.oracleparameter as oracle_param
 
-def test_oraclequerywithparams():
+def test_oraclequery():
     try:
         _oracle_utils =  oracle_utils.OracleUtils("SAMPLEDB_ADMIN", "localhost", "xe", "SAMPLEDB_ADMIN", "fighter70")
         """
         Oracle Queries without Params
         """
         columns, rows = _oracle_utils.execute("SELECT * FROM SAMPLEDB_ADMIN.CUSTOMERS")
-        """
-        Oracle Queries with Params
-        """
-        #params = {'isActive':'Y'}
-        #columns, rows = _oracle_utils.execute("SELECT * FROM AWP.LU_APPLICATION WHERE IS_APPLICATION_ACTIVE=:isActive", params)
     
         print('===================Columns===================')
         for x in range(len(columns)): 
@@ -30,8 +25,87 @@ def test_oraclequerywithparams():
         print('===================Rows===================')
         for row in rows:  
             print('--------------------ROW---------------------------' )
-            print(' APPLICATION_CODE :  ' + str(row['CUSTOMER_ID']))
-            print(' APPLICATION_CODE :  ' + row['CUSTOMER_NAME'])
-            print(' APPLICATION_SHORT_DESC :  ' + row['CITY'])
+            print(' CUSTOMER_ID :  ' + str(row['CUSTOMER_ID']))
+            print(' CUSTOMER_NAME :  ' + row['CUSTOMER_NAME'])
+            print(' CITY :  ' + row['CITY'])
     except : 
         print('Something went wrong. check Log')
+
+def test_oraclequerywithparams():
+    try:
+        _oracle_utils =  oracle_utils.OracleUtils("SAMPLEDB_ADMIN", "localhost", "xe", "SAMPLEDB_ADMIN", "fighter70")
+        """
+        Oracle Queries with Params
+        """
+        params = {'CITY_PARAM':'edison'}
+        columns, rows = _oracle_utils.execute("SELECT * FROM SAMPLEDB_ADMIN.CUSTOMERS WHERE CITY=:CITY_PARAM", params)
+
+        print('===================Rows===================')
+        for row in rows:  
+            print('--------------------ROW---------------------------' )
+            print(' CUSTOMER_ID :  ' + str(row['CUSTOMER_ID']))
+            print(' CUSTOMER_NAME :  ' + row['CUSTOMER_NAME'])
+            print(' CITY :  ' + row['CITY'])
+    except : 
+        print('Something went wrong. check Log')
+
+def test_oracleupdatequery():
+    try:
+        _oracle_utils =  oracle_utils.OracleUtils("SAMPLEDB_ADMIN", "localhost", "xe", "SAMPLEDB_ADMIN", "fighter70")
+        sqlquery = "update SAMPLEDB_ADMIN.CUSTOMERS set COMMENTS=:COMMENTS_PARAM where CUSTOMER_ID=:CUSTOMER_ID_PARAM"
+        params = {'CUSTOMER_ID':2, 'CUSTOMER_ID_PARAM':"raj test message"}
+        recordsupdated = _oracle_utils.execute_nonquery(sqlquery, params, true)
+        print(' recordsupdated:  ' + str(recordsupdated))
+    except : 
+        print('Something went wrong. check Log')
+
+def test_oraclequerywithparams():
+    try:
+        _oracle_utils =  oracle_utils.OracleUtils("SAMPLEDB_ADMIN", "localhost", "xe", "SAMPLEDB_ADMIN", "fighter70")
+        """
+        Oracle Queries with Params
+        """
+        params = {'CITY_PARAM':'edison'}
+        columns, rows = _oracle_utils.execute("SELECT * FROM SAMPLEDB_ADMIN.CUSTOMERS WHERE CITY=:CITY_PARAM", params)
+
+        print('===================Rows===================')
+        for row in rows:  
+            print('--------------------ROW---------------------------' )
+            print(' CUSTOMER_ID :  ' + str(row['CUSTOMER_ID']))
+            print(' CUSTOMER_NAME :  ' + row['CUSTOMER_NAME'])
+            print(' CITY :  ' + row['CITY'])
+    except : 
+        print('Something went wrong. check Log')
+
+def test_oracleprocedure():
+    """
+        Oracle Procedure Calls with Binding
+    """
+    try:
+        _oracle_utils =  oracle_utils.OracleUtils("SAMPLEDB_ADMIN", "localhost", "xe", "SAMPLEDB_ADMIN", "fighter70")
+        params = [] #parameters are positional 
+    
+        #One input Parameter and Output RefCursor
+        params.append(oracle_param.OracleParameter("p_city", "string", "input", "edison"))
+        params.append(oracle_param.OracleParameter("p_cursor", "refcursor", "output", None))
+        columns, rows = _oracle_utils.execute_proc("SAMPLEDB_ADMIN.PKG_CUSTOMERS_API.p_get_customers", params)
+        print('===================Columns===================')
+        if (columns != None ):
+            for x in range(len(columns)): 
+                print(' Name :  ' + columns[x].name)
+                print(' Type :  ' + str(columns[x].type))
+                print(' byte_size :  ' + str(columns[x].byte_size))
+                print(' internal_size :  ' + str(columns[x].internal_size))
+                print(' internal_size :  ' + str(columns[x].internal_size))
+                print(' precision :  ' + str(columns[x].precision))
+                print(' scale :  ' + str(columns[x].scale))
+                print(' nullable :  ' + str(columns[x].nullable))
+            print('===================Rows===================')
+            for row in rows:  
+                print('--------------------ROW---------------------------' )
+                print(' CUSTOMER_ID :  ' + str(row['CUSTOMER_ID']))
+                print(' CUSTOMER_NAME :  ' + row['CUSTOMER_NAME'])
+                print(' CITY :  ' + row['CITY'])
+    except Exception as e : 
+        print('Something went wrong. check Log')
+        print(str(e))
